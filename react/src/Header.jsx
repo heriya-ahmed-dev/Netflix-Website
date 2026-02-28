257baede0b54801d1f00aab81c1d5daf573d6081
Heracitta <heriyaahmedsiraje@gmail.com>
2026-02-28 08:56:41 +0300

    Updated file

--- react/src/Header.jsx
+++ react/src/Header.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './css/Header.css';
+import NetflixLogo from './assets/download.jpg';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ContactsIcon from '@mui/icons-material/Contacts';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='ThirdHorizontal'>
+        <div className='Horizontal'>
+        
+            <img src={NetflixLogo}/>
+      <li>Home</li>
+      <li>TVShows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+      <li>MyList</li>
+      <li>Browse My Languages</li>
+      <div className='Seclist'>
+          <SearchIcon/>
+           <NotificationsNoneIcon/>
+           <ContactsIcon/>
+           <ArrowDropDownIcon/>
+      </div>
+
+        </div>
 
 
-    
+    </div>
+
+
+
   )
 }
 
-export default Header
+export default Header;
 
